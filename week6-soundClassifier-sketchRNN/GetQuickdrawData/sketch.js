dbf348a10d5a62a4bb463e8c197bfa2d4a27c6f6
Yining Shi <ys2189@nyu.edu>
2019-03-20 12:35:19 -0400

    download 5000 of any category images from quickdraw dataset

--- week6-soundClassifier-sketchRNN/GetQuickdrawData/sketch.js
+++ week6-soundClassifier-sketchRNN/GetQuickdrawData/sketch.js
@@ -1,32 +1,38 @@
-/**
- * Based on Dan Shiffman's
-https://github.com/shiffman/Tensorflow-JS-Examples/tree/master/03_DoodleClassifier
-**/
-
-// This sketch allows you to download 1000 cat images
+// This sketch allows you to download 5000 images in one category from quickdraw dataset
+// ***NOTE***: When running this sketch locally, download this chrome extension to allow CORS
+// to be able to download files from Google storage
+// Download the chrome extension here: https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en
 const IMAGE_SIZE = 784;
-let catsData;
+const IMAGE_WIDTH = 28;
+const IMAGE_HEIGHT = 28;
+const OBJECT_NAME = 'lollipop';
+const numCols = 100;
+const numRows = 50;
+const totalImgs = 5000; // numCols * numRows has to = totalImgs
+// You can find the link to other categories here: https://console.cloud.google.com/storage/browser/quickdraw_dataset/full/numpy_bitmap/
+const fileName = 'https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/lollipop.npy';
+// Update the above variables to fit into your case
+let imagesData;
 let data;
 let allImgs = [];
 
 function preload() {
-  catsData = loadBytes('data/cats1000.bin');
+  imagesData = loadBytes(fileName);
 }
 
 function setup() {
-  createCanvas(2800, 280);
-  const total = catsData.bytes.length;
+  createCanvas(IMAGE_WIDTH * numCols, IMAGE_HEIGHT * numRows);
+  const total = imagesData.bytes.length;
   data = new Uint8Array(total);
-  data.set(catsData.bytes, 0);
-  for (let h = 0; h < 10; h++) {
-    for (let w = 0; w < 100; w++) {
-      const start = (w + h * 28) * 784;
-      createCatImage(start, w * 28, h * 28);
+  data.set(imagesData.bytes, 0);
+  for (let h = 0; h < numRows; h++) {
+    for (let w = 0; w < numCols; w++) {
+      const start = 80 + (w + h * numCols) * IMAGE_SIZE; // quickdraw dataset the real pixel data start from index 80
+      createOneImage(start, w * IMAGE_WIDTH, h * IMAGE_HEIGHT);
     }
   }
 
-  let mainButton = createButton(`download 1000 images as a zip file`);
-  mainButton.position(20, 380);
+  let mainButton = select("#downloadImg");
   mainButton.mousePressed(downloadAllImg);
 }
 
@@ -35,12 +41,12 @@ function downloadAllImg() {
 
   allImgs.forEach(async(im, index) => {
     const blob = await imageDataToBlob(im.imageData);
-    zip.file(`cat_${index}.png`, blob);
+    zip.file(`${OBJECT_NAME}_${index}.png`, blob);
 
-    if (index === 999) {
+    if (index === totalImgs - 1) {
       zip.generateAsync({ type:"blob" })
       .then(function(content) {
-        saveAs(content, "cats.zip");
+        saveAs(content, `${OBJECT_NAME}s.zip`);
       });
     }
   })
@@ -60,12 +66,12 @@ function imageDataToBlob(imageData) {
   });
 };
 
-function createCatImage(start, wIndex, hIndex) {
-  let img = createImage(28, 28);
+function createOneImage(start, wIndex, hIndex) {
+  let img = createImage(IMAGE_WIDTH, IMAGE_HEIGHT);
   img.loadPixels();
   for (let y = 0; y < img.height; y++) {
     for (let x = 0; x < img.width; x++) {
-      const color = data[start + x + y * 28];
+      const color = data[start + x + y * IMAGE_WIDTH];
       img.set(x, y, [255 - color, 255 - color, 255 - color, 255]);
     }
   }
