3e38012a18263c71e6da41abf445b03098efdac0
Yining Shi <shiyining1023@gmail.com>
2019-11-05 19:53:50 -0500

    added some comment

--- week9-doodleclassifier/GetQuickdrawData/trim.py
+++ week9-doodleclassifier/GetQuickdrawData/trim.py
@@ -1,3 +1,7 @@
+# You can run this script to trim the full_numpy_bitmap_lollipop.npy file
+# into a lollipop5000.npy that only has 5000 images
+# python trim.py
+
 import numpy as np
 
 data = np.load('data/full_numpy_bitmap_lollipop.npy')
