dc2177043ee7e001f727ab09d98e4b1b34861b44
Yining Shi <shiyining1023@gmail.com>
2020-10-04 20:04:55 -0400

    Added teachable mahine sound with arduino example

--- week4-soundClassifier/teachableMachineArduino-sound/arduino_code/arduino_code.ino
+++ week4-soundClassifier/teachableMachineArduino-sound/arduino_code/arduino_code.ino
@@ -1,16 +1,34 @@
-int ledPin = 13;
+int ledPin1 = 2;
+int ledPin2 = 3;
+int ledPin3 = 4;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);  // sets the pin as output
+  pinMode(ledPin1, OUTPUT);  // sets the pin as output
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
   Serial.begin(9600);           // initialize serial communications
 }
- 
+
 void loop() {
- if (Serial.available() > 0) { // if there's serial data available
-   int inByte = Serial.read();   // read it
-   Serial.write(inByte);         // send it back out as raw binary data
-   analogWrite(ledPin, inByte);       // use it to set the LED brightness
-   delay(2000);                // waits for 2 seconds
-   analogWrite(ledPin, 0);  // sets the LED off
- }
+  if (Serial.available() > 0) { // if there's serial data available
+    int inByte = Serial.read();   // read it
+    if (inByte == 1) {
+      digitalWrite(ledPin1, HIGH);
+      digitalWrite(ledPin2, LOW);
+      digitalWrite(ledPin3, LOW);
+    }else if (inByte == 2) {
+      digitalWrite(ledPin2, HIGH);
+      digitalWrite(ledPin1, LOW);
+      digitalWrite(ledPin3, LOW);
+    } else if (inByte == 3) {
+      digitalWrite(ledPin3, HIGH);
+      digitalWrite(ledPin1, LOW);
+      digitalWrite(ledPin2, LOW);
+    } else {
+      // Turn off all LEDs
+      digitalWrite(ledPin1, LOW);
+      digitalWrite(ledPin2, LOW);
+      digitalWrite(ledPin3, LOW);
+    }
+  }
 }
--- week4-soundClassifier/teachableMachineArduino-sound/p5_code/sketch.js
+++ week4-soundClassifier/teachableMachineArduino-sound/p5_code/sketch.js
@@ -1,8 +1,8 @@
-const mySoundModelURL = 'https://storage.googleapis.com/tm-speech-commands/yiningtestsound07112019/model.json';
+const mySoundModelURL = 'https://teachablemachine.withgoogle.com/models/t8338dy7Q/' + 'model.json';
 let mySoundModel;
 let resultDiv;
 let serial;          // variable to hold an instance of the serialport library
-let portName = '/dev/cu.usbmodem1411'; // fill in your serial port name here
+let portName = '/dev/cu.usbmodem142301'; // fill in your serial port name here
 let outByte = 0;                       // for outgoing data
 
 function preload() {
@@ -10,11 +10,19 @@ function preload() {
 }
 
 function setup() {
+  createCanvas(150, 150);
   resultDiv = createElement('h1',  '...');
   serial = new p5.SerialPort();    // make a new instance of the serialport library
   serial.on('error', serialError); // callback for errors
   serial.open(portName);           // open a serial port
   mySoundModel.classify(gotResults);
+}
+
+function draw() {
+  if (outByte === 0) background(255, 255, 255);
+  if (outByte === 1) background(255, 255, 0);
+  if (outByte === 2) background(0, 255, 0);
+  if (outByte === 3) background(255, 0, 0);
 }
 
 function serialError(err) {
@@ -26,8 +34,11 @@ function gotResults(err, results) {
   if (results) {
     console.log(results);
     resultDiv.html('Result is: ' + results[0].label);
-    if (results[0].label === 'Happy') {
-      outByte = 255;
+    if (results[0]) {
+      if (results[0].label === 'Ready') outByte = 1;
+      else if (results[0].label === 'Go') outByte = 2;
+      else if (results[0].label === 'Stop') outByte = 3;
+      else outByte = 0;
       // send it out the serial port:
       console.log('outByte: ', outByte)
       serial.write(outByte);
